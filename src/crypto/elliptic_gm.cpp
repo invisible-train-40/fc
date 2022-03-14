bab7a2e99e564cec841bc65e291b3858ecc36f22
cf <carter@carter.at>
2022-03-15 06:38:58 +0800

    ser

--- src/crypto/elliptic_gm.cpp
+++ src/crypto/elliptic_gm.cpp
@@ -90,6 +90,31 @@ namespace fc
 
         FC_THROW_EXCEPTION(exception, "unable to reconstruct public key from signature");
       }
+
+
+    std::string public_key::to_base58() const
+    {
+      public_key_data key = serialize();
+      uint32_t check = (uint32_t)sha256::hash(key.data, sizeof(key))._hash[0];
+      static_assert(sizeof(key) + sizeof(check) == 37, ""); // hack around gcc bug: key.size() should be constexpr, but isn't
+      array<char, 37> data;
+      memcpy(data.data, key.begin(), key.size());
+      memcpy(data.begin() + key.size(), (const char*)&check, sizeof(check));
+      return fc::to_base58(data.begin(), data.size(), fc::yield_function_t());
+    }
+
+    public_key public_key::from_base58( const std::string& b58 )
+    {
+        array<char, 37> data;
+        size_t s = fc::from_base58(b58, (char*)&data, sizeof(data) );
+        FC_ASSERT( s == sizeof(data) );
+
+        public_key_data key;
+        uint32_t check = (uint32_t)sha256::hash(data.data, sizeof(key))._hash[0];
+        FC_ASSERT( memcmp( (char*)&check, data.data + sizeof(key), sizeof(check) ) == 0 );
+        memcpy( (char*)key.data, data.data, sizeof(key) );
+        return public_key(key);
+    }
       public_key::public_key(const gm::sig_type &c, const fc::sha256 &digest, bool)
       {
         const unsigned char *dat = (unsigned char *)&c.data[0];
